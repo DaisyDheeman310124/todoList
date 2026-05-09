de1b04274ecc7f30a73bc412de1fc40bfbf4d173
Amit Kumar <amitkumar310124@gmail.com>
2026-05-09 23:50:55 +0530

    CORS Localhost Support: Added Credentials & Origin Headers

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ const ADMIN_EMAIL = 'amitkumar310124@gmail.com';
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     
     // Handle OPTIONS Preflight
     if (req.method === 'OPTIONS') {
